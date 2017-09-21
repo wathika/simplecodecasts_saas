cd0ee9cd5368f8760d336a3cacdf4e478d451cee
wathika <vcwathika@gmail.com>
2017-09-22 02:21:53 +0300

    added stripe js

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  Stripe.setPublishableKey($('meta[name="stripe_key"]').attr('content'));
+
+
+  //Watch for a form submission
+  $("pro_form_submit_btn").click(function(event){
+    event.preventDefault();
+    $('input[type=submit]').prop('disabled', true);
+    var error = alse;
+    var ccNum = $('#card_number').val(),
+        cvvNum = $('#card_code').val(),
+        expMonth = $('#card_month'),
+        expYear = $('#card_year');
+
+    if (!error) {
+      //Get the Stripe token
+      Stripe.createToken({
+        number: ccNum,
+        cvv: cvvNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeRespondHandler);
+    }
+    return false;
+
+  }); //form submission
+
+function stripeRespondHandler(status, esponse){
+  //Get a referenceto the form
+  var f = $("new_user");
+
+  //Get a token from the response
+  var token = response.id;
+
+  //Add the token to the form
+  f.append("<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />");
+
+  //Submit the form
+  f.get(0).submit();
+}
+
+})
