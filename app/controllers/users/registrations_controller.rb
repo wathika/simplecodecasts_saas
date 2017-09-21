509318c16646c33a01e922cbd2e22405598de3c5
wathika <vcwathika@gmail.com>
2017-09-22 02:53:53 +0300

    modified devise for stripe

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,15 @@
+class Users::RegisrationController < Devise::RegisrationController
+
+  def create
+    super do |resource|
+      if params[:plan]
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 2
+            resource.save_with_payment
+        else
+          resource.save
+        end
+    end
+  end
+
+end
