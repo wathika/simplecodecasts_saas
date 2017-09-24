3e2ac034e375c46e4dd36743871c60630b55cfd6
wathika <vcwathika@gmail.com>
2017-09-25 01:45:57 +0300

    added profile new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,10 @@
 class ProfilesController < ApplicationController
+  def new
+    #form for user to fill own profile
+    #identify user
+    @user = User.find(params[:user_id])
+    #prepare form
+    @profile = @user.build_profile
 
+  end
 end
